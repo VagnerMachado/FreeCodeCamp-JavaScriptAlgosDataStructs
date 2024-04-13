21991f23798230cbd85bccc8f46e9cf70265cf38
Vagner Machado <VagnerMachado84@hotmail.com>
2024-04-12 23:41:38 -0400

    work on coversion

--- 08. Recursion/script.js
+++ 08. Recursion/script.js
@@ -1,13 +1,32 @@
+
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+const decimalToBinary = (input) => {
+  let binary = "";
 
+  if (input === 0) {
+    binary = "0";
   }
 
-  console.log(numberInput.value);
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
