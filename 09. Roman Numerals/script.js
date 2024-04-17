cee9ec024703133080e4be658f6628e40b3196bf
Vagner Machado <VagnerMachado84@hotmail.com>
2024-04-16 22:55:25 -0400

    work on converter

--- 09. Roman Numerals/script.js
+++ 09. Roman Numerals/script.js
@@ -1,10 +1,9 @@
-const input = document.getElementById("number")
-const output = document.getElementById("output")
+const inputText = document.getElementById("number")
+const outputDiv = document.getElementById("output")
+const button = document.getElementById("convert-btn")
 
-output.textContent = "got it" + input.ariaValueMax;
 const val = new Map();
-
-val.set(1 , [ "I", "X", "C", "M"])
+val.set(1 , [ "I",  "X", "C", "M"])
 val.set(2 , [ "II", "XX", "CC", "MM"])
 val.set(3 , [ "III", "XXX", "CCC", "MMM"])
 val.set(4 , [ "IV", "XL", "CD"])
@@ -12,27 +11,29 @@ val.set(5 , [ "V", "L", "D"])
 val.set(6 , [ "VI", "LX", "DC"])
 val.set(7 , [ "VII", "LXX", "DCC"])
 val.set(8 , [ "VIII", "LXXX", "DCCC"])
-val.set(9 , ["IX", "XC", "CM"])
+val.set(9 , [ "IX", "XC", "CM"])
 
-let userInput = "92195"
-let reverseInput = userInput.split('').reverse();
-let position = 0;
-let output = "";
-
-while (position < reverseInput.length)
-{
-    if (parseInt(userInput) > 3999)
-    {
-        window.alert("Enter val btw 1 and 3999")
-        position = reverseInput.length
-    }
-    else if (parseInt(reverseInput[position]) != 0)
-    {
-        output = val.get(parseInt(reverseInput[position]))[position] + output
+const convert = () => {
+    let userInput = parseInt(inputText.value);
+    let reverseInput = inputText.value.split('').reverse();
+    let position = 0;
+    let output = "";
+    if (inputText.value.length === 0)
+        output = "Please enter a valid number";
+    while (position < reverseInput.length) {
+        if (userInput <= 0) {
+            output = "Please enter a number greater than or equal to 1";
+            position = reverseInput.length
+        }
+        else if (userInput > 3999) {
+            output = "Please enter a number less than or equal to 3999";
+            position = reverseInput.length
+        }
+        else if (parseInt(reverseInput[position]) != 0) {
+            output = val.get(parseInt(reverseInput[position]))[position] + output
+        }
         position++;
     }
+    outputDiv.textContent = output;
 }
-
-if(output.length)
-    console.log("output:" + output)
-
+button.addEventListener("click", convert)
