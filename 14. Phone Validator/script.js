40251208774113df220c1ad0594920f474410d49
Vagner Machado <VagnerMachado84@hotmail.com>
2024-05-21 22:08:56 -0400

    start project

---
+++ 14. Phone Validator/script.js
@@ -0,0 +1,6 @@
+input = document.getElementById("user-input").value;
+output = document.getElementById("output");
+
+console.log("input: " + input);
+if (input != "null" && input != null) output.value = input;
+``;
