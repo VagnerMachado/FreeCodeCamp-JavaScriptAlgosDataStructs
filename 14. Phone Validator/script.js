9ea7b83bfd53688a071f86da967f0c4cf14d28ea
Vagner Machado <VagnerMachado84@hotmail.com>
2024-05-22 23:00:46 -0400

    misc changes

--- 14. Phone Validator/script.js
+++ 14. Phone Validator/script.js
@@ -1,6 +1,28 @@
-input = document.getElementById("user-input").value;
-output = document.getElementById("output");
+input = document.getElementById("user-input");
+output = document.getElementById("results-div");
+check = document.getElementById("check-btn");
+clear = document.getElementById("clear-btn");
 
-console.log("input: " + input);
-if (input != "null" && input != null) output.value = input;
-``;
+const validate_input = () => {
+  const inputValue = input.value;
+  if (inputValue === "" || inputValue === null || inputValue === "null") {
+    input.value = "";
+    window.alert("Please provide a phone number");
+    return;
+  }
+
+  const phoneNumberRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+  if (phoneNumberRegex.test(inputValue)) {
+    output.innerHTML = "Valid US number: " + inputValue;
+  } else {
+    output.innerHTML = "Invalid US number: " + inputValue;
+  }
+  input.value = "";
+};
+
+const clear_output = () => {
+  output.innerHTML = ""; // Clear the outputs
+};
+
+check.addEventListener("click", validate_input);
+clear.addEventListener("click", clear_output);
