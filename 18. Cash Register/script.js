0781ae364b7648e0d0b5cd9d88d7e803cafe0b28
Vagner Machado <VagnerMachado84@hotmail.com>
2024-07-09 22:37:18 -0400

    work on change making, logs

--- 18. Cash Register/script.js
+++ 18. Cash Register/script.js
@@ -11,6 +11,7 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
+const values = [0.01, 0.05, 0.1, 0.25, 1.0, 5.0, 10.0, 20.0, 100.0];
 const cashInput = document.getElementById("cash");
 const changeOutput = document.getElementById("change-due");
 const purchaseButton = document.getElementById("purchase-btn");
@@ -21,19 +22,32 @@ const calculateChange = () => {
   const changeDue = parseFloat(
     parseFloat(cashInput.value) - parseFloat(price)
   ).toFixed(2);
-
-  changeOutput.textContent = "CHANGE " + changeDue;
+  console.log("-----------------------------");
   console.log("Button was clicked");
   console.log("Cash Input: " + parseFloat(cashInput.value));
   console.log("Cash in Register: " + getCashInRegister());
   console.log("Change due: " + changeDue);
+  changeOutput.textContent = "CHANGE " + changeDue;
 
-  if (cashInRegister < changeDue)
+  if (cashInRegister < changeDue) {
     status.textContent = "Status: INSUFFICIENT_FUNDS";
-  else if (cashInRegister == changeDue) status.textContent = "Status: CLOSED";
-  else status.textContent = "Status: OPEN";
+    console.log("Not enough cash in register");
+  } else if (cashInRegister == changeDue) {
+    status.textContent = "Status: CLOSED";
+    console.log("Change is equal to cash in register");
+  } else if (makeChange() >= 0) {
+    status.textContent = "Status: OPEN";
+    console.log("Change can be made");
+  } else {
+    status.textContent = "Status: INSUFFICIENT_FUNDS";
+    console.log("Cannot make change");
+  }
 };
 
+const makeChange = () => {
+  let arrayPosition = 8;
+  let done = false;
+};
 const getCashInRegister = () => {
   let total = 0.0;
   for (let v of cid) {
