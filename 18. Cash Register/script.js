d1ecdab9bdda760b5ed41c93168a673ab1617844
Vagner Machado <VagnerMachado84@hotmail.com>
2024-07-15 23:26:18 -0400

    try rework so test cases pass

--- 18. Cash Register/script.js
+++ 18. Cash Register/script.js
@@ -22,9 +22,10 @@ console.log("cid: " + cid);
 console.log("copyCid: " + cidCopy);
 const register = document.getElementById("register");
 let cashInput = document.getElementById("cash");
-const changeOutput = document.getElementById("change-due");
+const changeDue = document.getElementById("change-due");
 const purchaseButton = document.getElementById("purchase-btn");
-const changeStatus = document.getElementById("status");
+const itemPrice = document.getElementById("price");
+itemPrice.textContent = `Price: ${(price / 100).toFixed(2)}`;
 
 // -------------------------------------------------------
 
@@ -43,61 +44,66 @@ const printRegister = () => {
   for (let v of cid.reverse()) {
     if (v[1] != 0) res += v[0] + ": $" + v[1] + " ";
   }
-  return res;
+  return res.substring(0, res.length - 1);
+};
+
+const updateRegister = () => {
+  register.innerHTML = cid
+    .map((item) => item[0] + " " + item[1] + "<br>")
+    .toString()
+    .split(",")
+    .join(" ");
 };
 
 // -------------------------------------------------------
 
 const calculateChange = () => {
   const cashInRegister = getCashInRegister();
-  const changeDue = cashInput.value * 100 - price;
+  const changeAmount = cashInput.value * 100 - price;
   console.log("-----------------------------");
   console.log("Button was clicked");
   console.log("Cash Input: " + cashInput.value * 100);
   console.log("Cash in Register: " + getCashInRegister());
-  console.log("Change due: " + changeDue);
+  console.log("Change due: " + changeAmount);
 
-  if (changeDue == 0) {
-    changeOutput.textContent = "No change due - customer paid with exact cash";
-  } else if (changeDue < 0) {
+  if (changeAmount == 0) {
+    changeDue.textContent = "No change due - customer paid with exact cash";
+    cashInput.value = "";
+  } else if (changeAmount < 0) {
     alert("Customer does not have enough money to purchase the item");
-  } else if (cashInRegister < changeDue) {
-    changeStatus.textContent = "Status: INSUFFICIENT_FUNDS";
+    cashInput.value = "";
+  } else if (cashInRegister < changeAmount) {
+    changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
+    cashInput.value = "";
     console.log("Not enough cash in register");
-  } else if (cashInRegister == changeDue) {
-    changeStatus.textContent = "Status: CLOSED" + printRegister();
+  } else if (cashInRegister == changeAmount) {
+    changeDue.textContent = "Status: CLOSED " + printRegister();
+    console.log("#Status: CLOSED " + printRegister() + "#");
+    cashInput.value = "";
     console.log("Change is equal to cash in register");
-  } else if (makeChange(changeDue) >= 0) {
+  } else if (makeChange(changeAmount) >= 0) {
     console.log("Change can be made");
     // assign cidCopy to cid
-    cid = [...cidCopy];
-    cid = cid.map((v, index) => {
-      console.log("INDEX: " + index);
-      return [
-        v[0],
-        index <= 3 ? (v[1] / 100).toFixed(2) : Math.trunc(v[1] / 100),
-      ];
-    });
     console.log("CID after change made: " + cid);
-    changeStatus.textContent = "Status: OPEN " + printRegister();
   } else {
-    changeStatus.textContent = "Status: INSUFFICIENT_FUNDS";
+    changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
     console.log("Cannot make change");
   }
   console.log("CID: " + cid);
 };
 
 // -------------------------------------------------------
-const makeChange = (changeDue) => {
+const makeChange = (changeAmount) => {
   let arrayPosition = 8;
   let partialChange = 0;
+  let fromDrawer = 0;
   let changeGiven = "";
   while (arrayPosition >= 0) {
+    fromDrawer = 0;
     while (
       cidCopy[arrayPosition][1] != 0 &&
-      partialChange + values[arrayPosition] <= changeDue
+      partialChange + values[arrayPosition] <= changeAmount
     ) {
-      let fromDrawer = 0;
       console.log(
         "Current Value of cid at " +
           values[arrayPosition] +
@@ -109,10 +115,29 @@ const makeChange = (changeDue) => {
       partialChange += values[arrayPosition];
       cidCopy[arrayPosition][1] -= values[arrayPosition];
     }
-    console.log("Change due " + changeDue);
+    if (fromDrawer > 0)
+      changeGiven +=
+        cidCopy[arrayPosition][0] + ": $" + fromDrawer / 100.0 + " ";
+    console.log("Change due " + changeAmount);
     console.log("Partial Change " + partialChange);
     console.log("Done adding " + values[arrayPosition]);
-    if (partialChange == changeDue) {
+    if (partialChange == changeAmount) {
+      cid = [...cidCopy];
+      cid = cid.map((v, index) => {
+        console.log("INDEX: " + index);
+        return [
+          v[0],
+          index <= 3 ? (v[1] / 100).toFixed(2) : Math.trunc(v[1] / 100),
+        ];
+      });
+      changeDue.textContent =
+        "Status: OPEN " + changeGiven.substring(0, changeGiven.length - 1);
+      console.log(
+        "#Status: OPEN " +
+          changeGiven.substring(0, changeGiven.length - 1) +
+          "#"
+      );
+      updateRegister();
       console.log("change was made");
       return partialChange;
     }
@@ -123,8 +148,7 @@ const makeChange = (changeDue) => {
 };
 
 purchaseButton.addEventListener("click", calculateChange);
-register.innerHTML = cid.map((v) => `${v} <br>`);
-
+updateRegister();
 //ready to commit  need to do for 7/5
 
 // Status: OPEN TWENTY: $60 TEN: $20 FIVE: $15 ONE: $1 QUARTER: $0.5 DIME: $0.2 PENNY: $0.04"
